64f4185a2026941e0ed9acf90e0d1a3cc6282f2c
Nikola Tomovic <ntomovic92@gmail.com>
2024-01-16 12:06:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMMAND="upload"
+COMMAND="docker run -v $(pwd):/usr/src/app/translized translized-cli upload"
 
 if [ ! -z "$INPUT_CONFIG_FILE" ]; then
   COMMAND="$COMMAND -c $INPUT_CONFIG_FILE"
